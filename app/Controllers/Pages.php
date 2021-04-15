b5e7937973d92a803d31c4e25e1c1be942051c5d
yustiandikma <tianokondank@rocketmail.com>
2021-04-15 13:52:44 +0700

    5.view : membuat dan memindahkan folder dan isi pafes di views

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -8,4 +8,9 @@ class Pages extends BaseController
     {
         return view('home');
     }
+
+    public function about()
+    {
+        return view('about');
+    }
 }
---
+++ app/Views/pages/about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+</head>
+
+<body>
+    <h1>About Me !</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ut rem. Voluptas voluptatem itaque molestias dolor eos blanditiis provident deserunt id facilis? Quis ducimus ratione consequatur quisquam, numquam aut itaque.</p>
+</body>
+
+</html>
